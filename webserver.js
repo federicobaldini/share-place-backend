90e069e4a549bde132c807307fdab09c3bd16778
federicobaldini <federico-baldini@hotmail.com>
2022-01-13 21:56:35 +0100

    added test code on webserver

---
+++ webserver.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import placesRoutes from "./routes/places-routes.js";
+
+const webserver = express();
+
+webserver.use(placesRoutes);
+
+webserver.listen(5000);
