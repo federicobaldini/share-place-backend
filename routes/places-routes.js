c8383ff9423b1471f9632b34eec6961710c97177
federico baldini <federico.baldini7@gmail.com>
2022-01-15 21:32:18 +0100

    added errors management

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -18,13 +18,27 @@ const PLACES = [
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
   const place = PLACES.find((p) => p.id === placeId);
-  res.json({ place });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    next(error);
+  } else {
+    res.json({ place });
+  }
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
   const place = PLACES.find((p) => p.creator === userId);
-  res.json({ place });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    next(error);
+  } else {
+    res.json({ place });
+  }
 });
 
 export default router;
--- webserver.js
+++ webserver.js
@@ -7,4 +7,14 @@ const webserver = express();
 
 webserver.use("/api/places", placesRoutes);
 
+webserver.use((error, req, res, next) => {
+  if (res.headerSent) {
+    next(error);
+  } else {
+    res
+      .status(error.code || 500)
+      .json({ message: error.message || "An unknow error occurred!" });
+  }
+});
+
 webserver.listen(5000);
