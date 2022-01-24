fc7b87a9c56715a1936db42ef5fd3a05ca3193a0
federicobaldini <federico-baldini@hotmail.com>
2022-01-24 13:08:33 +0100

    created and stored user document in the database

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,6 +2,7 @@ import { v4 as uuidV4 } from "uuid";
 import { validationResult } from "express-validator";
 
 import HttpError from "../models/http-error.js";
+import User from "../models/user.js";
 
 let USERS = [
   {
@@ -16,29 +17,52 @@ const getUsers = (req, res, next) => {
   res.json({ users: USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
-  const hasUser = USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError("Could not create user, email already exists.", 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
-  const createdUser = {
-    id: uuidV4(),
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, pleace login instead.",
+      422
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
     name: name,
     email: email,
+    image:
+      "https://images.pexels.com/photos/10430890/pexels-photo-10430890.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     password: password,
-  };
+    places: places,
+  });
 
-  USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
 
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
@@ -46,7 +70,7 @@ const login = (req, res, next) => {
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed, please check your data.", 422);
   }
-  
+
   const { email, password } = req.body;
 
   const identifiedUser = USERS.find((u) => u.email === email);
