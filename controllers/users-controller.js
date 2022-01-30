661a68ab2f6ab5f31419a9f671b6a8b7040f7e76
federicobaldini <federico-baldini@hotmail.com>
2022-01-30 15:13:56 +0100

    returned user id on login function

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -98,7 +98,7 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ message: "Logged in!" });
+  res.json({ user: existingUser.toObject({ getters: true }) });
 };
 
 const _getUsers = getUsers;
